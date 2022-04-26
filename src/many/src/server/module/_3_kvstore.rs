599f09c1b7995647105bab356913914e2c2375a7
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2022-04-26 09:24:24 -0400

    Call KvStore module endpoints directly

--- src/many/src/server/module/_3_kvstore.rs
+++ src/many/src/server/module/_3_kvstore.rs
@@ -16,22 +16,13 @@ pub trait KvStoreModuleBackend: Send {
 mod tests {
     use super::*;
     use minicbor::bytes::ByteVec;
-    use proptest::prelude::*;
 
     use std::{
         collections::BTreeMap,
         sync::{Arc, Mutex},
     };
 
-    use crate::{
-        message::RequestMessage,
-        message::{RequestMessageBuilder, ResponseMessage},
-        server::tests::execute_request,
-        types::identity::{cose::tests::generate_random_eddsa_identity, CoseKeyIdentity},
-        ManyServer,
-    };
-
-    const SERVER_VERSION: u8 = 1;
+    use crate::server::module::testutils::{call_module_cbor, call_module_cbor_diag};
 
     struct KvStoreImpl(pub BTreeMap<ByteVec, ByteVec>);
     impl std::default::Default for KvStoreImpl {
@@ -65,70 +56,30 @@ mod tests {
         }
     }
 
-    prop_compose! {
-        fn arb_server()(name in "\\PC*") -> (CoseKeyIdentity, Arc<Mutex<ManyServer>>) {
-            let id = generate_random_eddsa_identity();
-            let server = ManyServer::new(name, id.clone());
-            let kvstore_impl = Arc::new(Mutex::new(KvStoreImpl::default()));
-            let kvstore_module = KvStoreModule::new(kvstore_impl);
+    #[test]
+    fn info() {
+        let module_impl = Arc::new(Mutex::new(KvStoreImpl::default()));
+        let module = super::KvStoreModule::new(module_impl);
 
-            {
-                let mut s = server.lock().unwrap();
-                s.version = Some(SERVER_VERSION.to_string());
-                s.add_module(kvstore_module);
-            }
+        let info_returns: InfoReturns =
+            minicbor::decode(&call_module_cbor_diag(&module, "kvstore.info", "null").unwrap())
+                .unwrap();
 
-            (id, server)
-        }
+        assert_eq!(info_returns.hash, ByteVec::from(vec![1u8; 8]));
     }
 
-    proptest! {
-        #[test]
-        fn info((id, server) in arb_server()) {
-            let request: RequestMessage = RequestMessageBuilder::default()
-                .version(SERVER_VERSION)
-                .from(id.identity)
-                .to(id.identity)
-                .method("kvstore.info".to_string())
-                .data("null".as_bytes().to_vec())
-                .build()
-                .unwrap();
+    #[test]
+    fn get() {
+        let module_impl = Arc::new(Mutex::new(KvStoreImpl::default()));
+        let module = super::KvStoreModule::new(module_impl);
 
-            let response_message = execute_request(id, server, request);
+        let mut data = BTreeMap::new();
+        data.insert(0, ByteVec::from(vec![1, 3, 5, 7]));
+        let data = minicbor::to_vec(data).unwrap();
 
-            let bytes = response_message.to_bytes().unwrap();
-            let response_message: ResponseMessage = minicbor::decode(&bytes).unwrap();
+        let get_returns: GetReturns =
+            minicbor::decode(&call_module_cbor(&module, "kvstore.get", data).unwrap()).unwrap();
 
-            let bytes = response_message.data.unwrap();
-            let info_returns: InfoReturns = minicbor::decode(&bytes).unwrap();
-
-            assert_eq!(info_returns.hash, ByteVec::from(vec![1u8; 8]));
-        }
-
-        #[test]
-        fn get((id, server) in arb_server()) {
-            let mut data = BTreeMap::new();
-            data.insert(0, ByteVec::from(vec![1, 3, 5, 7]));
-            let data = minicbor::to_vec(data).unwrap();
-            let request: RequestMessage = RequestMessageBuilder::default()
-                .version(SERVER_VERSION)
-                .from(id.identity)
-                .to(id.identity)
-                .method("kvstore.get".to_string())
-                .data(data)
-                .build()
-                .unwrap();
-
-            let response_message = execute_request(id, server, request);
-
-            let bytes = response_message.to_bytes().unwrap();
-            let response_message: ResponseMessage = minicbor::decode(&bytes).unwrap();
-
-            let bytes = response_message.data.unwrap();
-
-            let get_returns: GetReturns = minicbor::decode(&bytes).unwrap();
-
-            assert_eq!(get_returns.value, Some(ByteVec::from(vec![1, 2, 3, 4])));
-        }
+        assert_eq!(get_returns.value, Some(ByteVec::from(vec![1, 2, 3, 4])));
     }
 }
