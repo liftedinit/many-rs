52527858eab5e4f85b19933399ed2e7fd53fe728
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2022-04-27 10:34:40 -0400

    Add CBOR encoding for kvstore InfoArgs

--- src/many/src/server/module/_3_kvstore/info.rs
+++ src/many/src/server/module/_3_kvstore/info.rs
@@ -1,10 +1,17 @@
 use minicbor::bytes::ByteVec;
-use minicbor::{decode, Decode, Decoder, Encode};
+use minicbor::encode::{Error, Write};
+use minicbor::{decode, Decode, Decoder, Encode, Encoder};
 
 pub struct InfoArgs;
 impl<'de> Decode<'de> for InfoArgs {
     fn decode(_d: &mut Decoder<'de>) -> Result<Self, decode::Error> {
         Ok(Self)
+    }
+}
+
+impl Encode for InfoArgs {
+    fn encode<W: Write>(&self, e: &mut Encoder<W>) -> Result<(), Error<W::Error>> {
+        e.null()?.ok()
     }
 }
 
