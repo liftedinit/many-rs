456e036889f703841234116b2de759b2e8c82257
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2022-04-27 10:48:16 -0400

    Implement CBOR encoder for LedgerTransaction TransactionsArgs

--- src/many/src/server/module/_4_ledger_transactions/info.rs
+++ src/many/src/server/module/_4_ledger_transactions/info.rs
@@ -1,9 +1,16 @@
-use minicbor::{decode, Decode, Decoder, Encode};
+use minicbor::encode::{Error, Write};
+use minicbor::{decode, Decode, Decoder, Encode, Encoder};
 
 pub struct TransactionsArgs;
 impl<'de> Decode<'de> for TransactionsArgs {
     fn decode(_d: &mut Decoder<'de>) -> Result<Self, decode::Error> {
         Ok(Self)
+    }
+}
+
+impl Encode for TransactionsArgs {
+    fn encode<W: Write>(&self, e: &mut Encoder<W>) -> Result<(), Error<W::Error>> {
+        e.null()?.ok()
     }
 }
 
