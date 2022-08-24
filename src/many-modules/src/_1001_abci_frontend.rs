c8d0cf59eb68e4112dae71823b5e1ba0d2c25ab1
Félix C. Morency <1102868+fmorency@users.noreply.github.com>
2022-08-24 10:41:39 -0400

    AbciFrontend module unit tests - `mockall` (#79)

--- src/many-modules/src/_1001_abci_frontend.rs
+++ src/many-modules/src/_1001_abci_frontend.rs
@@ -1,6 +1,11 @@
+use crate::EmptyReturn;
 use many_error::{define_attribute_many_error, ManyError};
 use many_macros::many_module;
-use minicbor::{Decode, Encode};
+
+#[cfg(test)]
+use mockall::{automock, predicate::*};
+
+pub type StatusReturn = EmptyReturn;
 
 define_attribute_many_error!(
     attribute 1001 => {
@@ -8,11 +13,27 @@ define_attribute_many_error!(
     }
 );
 
-#[derive(Clone, Debug, Encode, Decode)]
-#[cbor(map)]
-pub struct StatusReturns {}
-
 #[many_module(name = AbciFrontendModule, id = 1001, namespace = abci, many_modules_crate = crate)]
+#[cfg_attr(test, automock)]
 pub trait AbciClientModuleBackend: Send {
-    fn status(&self) -> Result<StatusReturns, ManyError>;
+    fn status(&self) -> Result<StatusReturn, ManyError>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::testutils::call_module;
+    use std::sync::{Arc, Mutex};
+
+    #[test]
+    fn status() {
+        let mut mock = MockAbciClientModuleBackend::new();
+        mock.expect_status()
+            .times(1)
+            .returning(|| Ok(StatusReturn {}));
+        let module = super::AbciFrontendModule::new(Arc::new(Mutex::new(mock)));
+
+        let _: StatusReturn =
+            minicbor::decode(&call_module(1, &module, "abci.status", "null").unwrap()).unwrap();
+    }
 }
