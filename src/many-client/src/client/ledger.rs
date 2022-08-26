ec66166eb5227f4f8746d0ef2df20dc902951551
Yuri Albuquerque <yuridenommus@gmail.com>
2022-08-26 14:43:45 -0300

    Fix send to return SendReturns

--- src/many-client/src/client/ledger.rs
+++ src/many-client/src/client/ledger.rs
@@ -1,5 +1,5 @@
 use many_client_macros::many_client;
-pub use many_modules::ledger::{BalanceArgs, BalanceReturns, InfoReturns, SendArgs};
+pub use many_modules::ledger::{BalanceArgs, BalanceReturns, InfoReturns, SendArgs, SendReturns};
 pub use many_types::ledger::{Symbol, TokenAmount};
 
 #[many_client(
@@ -7,7 +7,7 @@ pub use many_types::ledger::{Symbol, TokenAmount};
     methods(
         info(returns = "InfoReturns"),
         balance(params = "BalanceArgs", returns = "BalanceReturns"),
-        send(params = "SendArgs"),
+        send(params = "SendArgs", returns = "SendReturns"),
     )
 )]
 pub struct LedgerClient;
