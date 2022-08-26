749a9fd6f530506e6216791c916dc673b281d24c
Yuri Albuquerque <yuridenommus@gmail.com>
2022-08-26 12:44:05 -0300

    Fix formatting

--- src/many-client/src/client/base.rs
+++ src/many-client/src/client/base.rs
@@ -1,11 +1,9 @@
 use many_client_macros::many_client;
 use many_modules::base::{Endpoints, Status};
 
-#[many_client(
-    methods(
-        status(returns = "Status"),
-        heartbeat(),
-        endpoints(returns = "Endpoints"),
-    )
-)]
+#[many_client(methods(
+    status(returns = "Status"),
+    heartbeat(),
+    endpoints(returns = "Endpoints"),
+))]
 pub struct BaseClient;
