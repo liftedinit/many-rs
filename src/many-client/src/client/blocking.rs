aa46ec905709e8c6a45f8424aeb19bce922a98ec
Yuri Albuquerque <yuridenommus@gmail.com>
2022-09-08 16:00:01 -0300

    Optionally choosing an existing handle or creating a runtime

--- src/many-client/src/client/blocking.rs
+++ src/many-client/src/client/blocking.rs
@@ -6,37 +6,54 @@ use many_protocol::{RequestMessage, ResponseMessage};
 use many_server::ManyError;
 use minicbor::Encode;
 use reqwest::IntoUrl;
-use tokio::runtime::Runtime;
+use tokio::runtime::{self, Handle, Runtime};
 
 use crate::ManyClient as AsyncClient;
 
 #[derive(Debug, Clone)]
 pub struct ManyClient<I: Identity> {
     client: AsyncClient<I>,
-    runtime: Arc<Runtime>,
+    runtime: Option<Arc<Runtime>>,
+    handle: Option<Handle>,
 }
 
 impl<I: Identity> ManyClient<I> {
+    fn handle(&self) -> &Handle {
+        self.handle
+            .as_ref()
+            .unwrap_or_else(|| self.runtime.as_ref().unwrap().handle())
+    }
+
     pub fn new<S: IntoUrl>(url: S, to: Address, identity: I) -> Result<Self, String> {
         let client = AsyncClient::new(url, to, identity)?;
-        let runtime = Arc::new(
-            tokio::runtime::Builder::new_current_thread()
-                .enable_all()
-                .build()
-                .map_err(|e| e.to_string())?,
-        );
-        Ok(Self { client, runtime })
+        match Handle::try_current() {
+            Ok(h) => Ok(Self {
+                client,
+                runtime: None,
+                handle: Some(h),
+            }),
+            Err(_) => Ok(Self {
+                client,
+                runtime: Some(Arc::new(
+                    runtime::Builder::new_current_thread()
+                        .enable_all()
+                        .build()
+                        .map_err(|e| e.to_string())?,
+                )),
+                handle: None,
+            }),
+        }
     }
 
     pub fn send_message(&self, message: RequestMessage) -> Result<ResponseMessage, ManyError> {
-        self.runtime.block_on(self.client.send_message(message))
+        self.handle().block_on(self.client.send_message(message))
     }
 
     pub fn call_raw<M>(&self, method: M, argument: &[u8]) -> Result<ResponseMessage, ManyError>
     where
         M: Into<String>,
     {
-        self.runtime
+        self.handle()
             .block_on(self.client.call_raw(method, argument))
     }
 
@@ -45,7 +62,7 @@ impl<I: Identity> ManyClient<I> {
         M: Into<String>,
         A: Encode<()>,
     {
-        self.runtime.block_on(self.client.call(method, argument))
+        self.handle().block_on(self.client.call(method, argument))
     }
 
     pub fn call_<M, A>(&self, method: M, argument: A) -> Result<Vec<u8>, ManyError>
@@ -53,10 +70,10 @@ impl<I: Identity> ManyClient<I> {
         M: Into<String>,
         A: Encode<()>,
     {
-        self.runtime.block_on(self.client.call_(method, argument))
+        self.handle().block_on(self.client.call_(method, argument))
     }
 
     pub fn status(&self) -> Result<Status, ManyError> {
-        self.runtime.block_on(self.client.status())
+        self.handle().block_on(self.client.status())
     }
 }
