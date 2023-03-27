c5a75b819c478057b391529d1b1c4b81a7deecaf
Onyeka Obi <Onyeka.Obi@gmail.com>
2023-03-27 12:49:56 -0700

    To merk v2 for ledger db CLI

--- src/ledger-db/src/main.rs
+++ src/ledger-db/src/main.rs
@@ -1,9 +1,11 @@
-use clap::Parser;
-use derive_more::{From, TryInto};
-use many_types::ledger::TokenAmount;
-use merk_v1::rocksdb::{DBIterator, IteratorMode, ReadOptions};
-use merk_v1::tree::Tree;
-use std::path::{Path, PathBuf};
+use {
+    clap::Parser,
+    derive_more::{From, TryInto},
+    many_types::ledger::TokenAmount,
+    merk_v1::rocksdb::{IteratorMode, ReadOptions},
+    merk_v1::tree::Tree,
+    std::path::PathBuf,
+};
 
 #[derive(Parser)]
 struct Opts {
@@ -11,42 +13,25 @@ struct Opts {
     store: PathBuf,
 }
 
-enum Merk {
-    V1(merk_v1::Merk),
-    #[allow(dead_code)]
-    V2(merk_v2::Merk),
-}
+type InnerStorage = merk_v2::Merk;
 
 #[derive(Debug, From, TryInto)]
 enum Error {
-    V1(merk_v1::Error),
-    V2(merk_v2::Error),
+    Decode(minicbor::decode::Error),
+    EndOfIterator,
+    Merk(merk_v2::Error),
+    Rocks(merk_v2::rocksdb::Error),
 }
 
-impl Merk {
-    fn iter_opt(&self, mode: IteratorMode, options: ReadOptions) -> DBIterator {
-        match self {
-            Merk::V1(merk) => merk.iter_opt(mode, options),
-            Merk::V2(merk) => merk.iter_opt(mode, options),
-        }
-    }
-
-    fn open<P: AsRef<Path>>(path: P) -> Result<Self, Error> {
-        merk_v1::Merk::open(path).map(Self::V1).map_err(Into::into)
-    }
-}
-
-type InnerStorage = Merk;
-
-fn main() {
+fn main() -> Result<(), Error> {
     let Opts { store } = Opts::parse();
 
-    let merk = InnerStorage::open(store).expect("Could not open the store.");
+    let merk = InnerStorage::open(store)?;
 
     let it = merk.iter_opt(IteratorMode::Start, ReadOptions::default());
 
     for kv_result in it {
-        let (k, v) = kv_result.unwrap();
+        let (k, v) = kv_result?;
         let new_v = Tree::decode(k.to_vec(), v.as_ref());
 
         let k: Vec<u8> = k.into();
@@ -55,14 +40,17 @@ fn main() {
         // Try to "smartly" decode the key.
         if k.starts_with(b"/events/") {
             let k = hex::encode(&k[8..]);
-            let log = minicbor::decode::<many_modules::events::EventLog>(v).unwrap();
+            let log = minicbor::decode::<many_modules::events::EventLog>(v)?;
             println!("event {k} => {log:?}",)
         } else if k.starts_with(b"/balances/") {
             let k = &k[10..];
             // This should be utf8.
             let k = String::from_utf8_lossy(k);
             let mut it = k.split('/');
-            let (id, symbol) = (it.next().unwrap(), it.next().unwrap());
+            let (id, symbol) = it
+                .next()
+                .and_then(|id| it.next().map(|symbol| (id, symbol)))
+                .ok_or(Error::EndOfIterator)?;
             let t = TokenAmount::from(v.to_vec());
             println!("balance {id} => {t} {symbol}");
         } else if k.starts_with(b"/multisig/") {
@@ -75,4 +63,5 @@ fn main() {
             println!("unknown 0x {} => {}", hex::encode(k), hex::encode(v));
         }
     }
+    Ok(())
 }
