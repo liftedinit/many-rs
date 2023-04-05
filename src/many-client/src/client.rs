a5572bf1670a860e7f401d957c5a70a29300c963
Onyeka Obi <Onyeka.Obi@gmail.com>
2023-04-04 22:25:11 -0700

    Use modified reqwest repo for further debugging

--- src/many-client/src/client.rs
+++ src/many-client/src/client.rs
@@ -43,17 +43,13 @@ pub async fn send_envelope<S: IntoUrl>(url: S, message: CoseSign1) -> Result<Cos
 
     let client = reqwest::Client::new();
     tracing::debug!("request {}", hex::encode(&bytes));
-    //let response = client
-    //    .post(url)
-    //    .body(bytes)
-    //    .send()
-    //    .await
-    //    .map_err(|error| ManyError::unknown(error.to_string()))?;
-    let response = client.post(url).body(bytes).send().await;
-    println!("Reqwest response:");
-    println!("{response:?}");
-    let response = response.map_err(|error| ManyError::unknown(error.to_string()))?;
-    //.map_err(|e| ManyError::unexpected_transport_error(e.to_string()))?;
+    let response = client
+        .post(url)
+        .body(bytes)
+        .send()
+        .await
+        //.map_err(|e| ManyError::unexpected_transport_error(e.to_string()))?;
+        .unwrap();
     let body = response.bytes().await.unwrap();
     let bytes = body.to_vec();
     tracing::debug!("reply {}", hex::encode(&bytes));
